f7290714c4c6d557c6674fdde4f259c46c41c0b3
Tyuiu.AbdullinAI@outlook.com <holchik919@gmail.com>
2023-09-06 19:40:28 +0500

    Task1

---
+++ Tyuiu.AbdullinAI.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.AbdullinAI.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //writeline - выводит текст; Console - обращаемся к классу
+            string name = "Артур";
+            /*            string -строка; name - переменная*/
+            int age = 17;
+            //int - задает числовое значение
+            Console.WriteLine("здравствуйте! Меня зовут " + name + " и мне " + age + " лет) "); 
+            Console.ReadKey();
+            //readkey - метод; ждет нажатия клавиши, чтобы мы увидели надпись
+
+        }
+    }
+}
