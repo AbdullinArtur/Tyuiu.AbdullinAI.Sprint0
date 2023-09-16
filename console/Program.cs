02b4f91b83287346ff43863b4b38c2356ce8035e
Tyuiu.AbdullinAI@outlook.com <holchik919@gmail.com>
2023-09-16 13:25:53 +0500

    Task3

---
+++ console/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");         
+            string name = "артур";
+            int age = 17;
+            Console.WriteLine("привет, меня зовут " + name + " и мне " + age + " лет");
+            int lol = 16;
+            Console.WriteLine(lol);
+            Console.ReadKey();
+            
+            
+
+
+
+
+
+
+        }
+    }
+}
