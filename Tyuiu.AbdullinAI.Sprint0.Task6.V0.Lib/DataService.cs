841955347037e8be577f3238dac7c5968a5ff2ab
Tyuiu.AbdullinAI@outlook.com <holchik919@gmail.com>
2023-09-17 18:16:49 +0500

    Task6

---
+++ Tyuiu.AbdullinAI.Sprint0.Task6.V0.Lib/DataService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.AbdullinAI.Sprint0.Task6.V0.Lib
+{
+    public class DataService
+    {
+        /* Пример циклической структуры (цикл с параметрами)    for */
+        public static object AdditionArray(int[] numbers)
+        {
+            var total = 0;
+            for (var i = 0; i < numbers.Length; i++) /*i < numbers.Length; - до длины массива; i++ - инкримент(увеличивает значение) */
+            {
+                total = total + numbers[i];
+            }
+            return total;
+        }
+        /* Пример циклической структуры (цикл с предусловием) while */
+        
+        public static object SubstractionArray(int[] numbers)
+        {
+            var total = 0;
+            int index = 0;
+            while (index < numbers.Length)
+            {
+                total = total - numbers[index];
+                index++;
+            }
+            return total;
+        }
+        /* Пример циклической структуры (цикл с постусловием) do/while  */
+        
+        public static object MultiplicationArray(int[] numbers)
+        {
+            var total = 1;
+            int index = 0;
+            do
+            {
+                total = total * numbers[index];
+                index++;
+            }
+            while (index < numbers.Length);
+            return total;
+        }
+    }
+}   
---
+++ Tyuiu.AbdullinAI.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.AbdullinAI.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.AbdullinAI.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("19df120f-0e3d-4cc9-9ef4-0ecc7fe27ca1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
